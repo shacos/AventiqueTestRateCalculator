2612ca0989ac0a67e4e73a3f7270c5b29aace744
Mehdi Chikhi <ci_mi@outlook.fr>
2025-12-02 18:07:51 +0100

    Initial Commit

---
+++ TestAventique/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestAventique
+//
+//  Created by Mehdi on 2/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestAventique/TestAventiqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAventiqueApp.swift
+//  TestAventique
+//
+//  Created by Mehdi on 2/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestAventiqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
